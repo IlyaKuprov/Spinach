429fa0c2b9df0bdc24c4944888529612f67376fd
anu-acharya98 <a.acharya@soton.ac.uk>
2024-04-22 19:53:08 +0100

    - Heterodyne function improved to take just time_grid and voltage data as arrays - Outputs are in-phase and out of phase parts of the signal. - Plotting functionality removed. - Optional line added to check if user has gpu access, fft filter works on gpu if this line = yes.

---
+++ kernel/heterodyne.m
@@ -0,0 +1,54 @@
+% Filters out high frequency signals for pulse waveform as recorded by an
+% oscilloscope. Supports GPU usage. Syntax:
+% 
+%                heterodyne(time_grid,exp_data,carrier_freq)
+%
+% Parameters:
+%
+%   time_grid      -  Time grid supplied by RLC circuit data,
+%                     a column vector.
+%
+%   exp_data       -  Amplitude data, supplied as a column vector.
+%
+%   carrier_freq   -  carrier frequency of the instrument
+%
+% Outputs:
+%
+%       X        - in-phase part of the rotating frame
+%                  pulse waveform distorted by the RLC
+%                  response, a column vector of real 
+%                  numbers
+%
+%       Y        - out-of-phase part of the rotating
+%                  frame pulse waveform distorted by
+%                  the RLC response, a column vector
+%                  of real numbers
+
+function [X,Y]=heterodyne(time_grid,exp_data,carrier_freq)
+
+% Get carrier frequency in rad/s
+omega=2*pi*carrier_freq;
+
+% Mix with carrier frequency
+X=2*exp_data.*cos(omega*time_grid);
+Y=2*exp_data.*sin(omega*time_grid);
+
+% Define filter
+d=designfilt('lowpassfir','SampleRate',2.5e9, ...
+             'PassbandFrequency',1e6,'StopbandFrequency',1.5e7, ...
+             'PassbandRipple',0.5,'StopbandAttenuation',50);
+B = d.Coefficients;
+
+% Use GPU if available
+if canUseGPU()
+    X=gather(fftfilt(gpuArray(B),gpuArray(X)));
+    Y=gather(fftfilt(gpuArray(B),gpuArray(Y)));
+else 
+
+% Apply filter on CPU
+X=fftfilt(B,X);
+Y=fftfilt(B,Y);
+
+end 
+
+end

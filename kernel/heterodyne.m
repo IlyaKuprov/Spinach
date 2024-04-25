36b26181f6440153e12936fe5a7a16a30b47dd9a
anu-acharya98 <a.acharya@soton.ac.uk>
2024-04-25 14:48:21 +0100

    Now also does mixing with carrier frequency on the GPU.

--- kernel/heterodyne.m
+++ kernel/heterodyne.m
@@ -29,9 +29,14 @@ function [X,Y]=heterodyne(time_grid,exp_data,carrier_freq)
 % Get carrier frequency in rad/s
 omega=2*pi*carrier_freq;
 
-% Mix with carrier frequency
-X=2*exp_data.*cos(omega*time_grid);
-Y=2*exp_data.*sin(omega*time_grid);
+% Mix with carrier frequency on GPU if possible 
+if canUseGPU()
+    X=gather(2*gpuArray(exp_data).*cos(omega*gpuArray(time_grid)));
+    Y=gather(2*gpuArray(exp_data).*sin(omega*gpuArray(time_grid)));
+else
+    X=2*exp_data.*cos(omega*time_grid);
+    Y=2*exp_data.*sin(omega*time_grid);
+end
 
 % Define filter
 d=designfilt('lowpassfir','SampleRate',2.5e9, ...
@@ -39,7 +44,7 @@ d=designfilt('lowpassfir','SampleRate',2.5e9, ...
              'PassbandRipple',0.5,'StopbandAttenuation',50);
 B = d.Coefficients;
 
-% Use GPU if available
+% Apply filter on GPU if available
 if canUseGPU()
     X=gather(fftfilt(gpuArray(B),gpuArray(X)));
     Y=gather(fftfilt(gpuArray(B),gpuArray(Y)));

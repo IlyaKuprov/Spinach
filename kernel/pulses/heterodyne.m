d31800a70153967b42e428c4166545f91722e03a
Ilya Kuprov <i.kuprov@outlook.com>
2024-04-28 18:44:21 +0100

    Over to Anu for magic number elimination.

--- kernel/pulses/heterodyne.m
+++ kernel/pulses/heterodyne.m
@@ -10,7 +10,7 @@
 %
 %   exp_data       -  Amplitude data, supplied as a column vector.
 %
-%   carrier_freq   -  carrier frequency of the instrument
+%   carrier_freq   -  carrier frequency, Hz
 %
 % Outputs:
 %
@@ -26,34 +26,30 @@
 
 function [X,Y]=heterodyne(time_grid,exp_data,carrier_freq)
 
-% Get carrier frequency in rad/s
+% Needs a grumbler
+
+% Carrier frequency to rad/s
 omega=2*pi*carrier_freq;
 
-% Mix with carrier frequency on GPU if possible 
+% Move inputs to GPU
 if canUseGPU()
-    X=gather(2*gpuArray(exp_data).*cos(omega*gpuArray(time_grid)));
-    Y=gather(2*gpuArray(exp_data).*sin(omega*gpuArray(time_grid)));
-else
-    X=2*exp_data.*cos(omega*time_grid);
-    Y=2*exp_data.*sin(omega*time_grid);
+    exp_data=gpuArray(exp_data); time_grid=gpuArray(time_grid);
 end
 
-% Define filter
-d=designfilt('lowpassfir','SampleRate',2.5e9, ...
-             'PassbandFrequency',1e6,'StopbandFrequency',1.5e7, ...
-             'PassbandRipple',0.5,'StopbandAttenuation',50);
-B = d.Coefficients;
+% Mix with carrier frequency
+X=2*exp_data.*cos(omega*time_grid);
+Y=2*exp_data.*sin(omega*time_grid);
+clear('exp_data','time_grid');
 
-% Apply filter on GPU if available
-if canUseGPU()
-    X=gather(fftfilt(gpuArray(B),gpuArray(X)));
-    Y=gather(fftfilt(gpuArray(B),gpuArray(Y)));
-else 
+% Define a lowpass filter
+d=designfilt('lowpassfir','SampleRate',          2.5e9, ...
+                          'PassbandFrequency',   1e6,   ...
+                          'StopbandFrequency',   1.5e7, ...
+                          'PassbandRipple',      0.5,   ...
+                          'StopbandAttenuation', 50);
+B = d.Coefficients; if canUseGPU(), B=gpuArray(B); end
 
-% Apply filter on CPU
-X=fftfilt(B,X);
-Y=fftfilt(B,Y);
-
-end 
+% Apply the lowpass filter
+X=gather(fftfilt(B,X)); Y=gather(fftfilt(B,Y));
 
 end

5764cdb24a995bf0e141266218fdc9ca8aead03e
Ilya Kuprov <ilya.kuprov@weizmann.ac.il>
2025-02-10 10:37:07 +0200

    First round of tidying up for Maxi's TDHAM code.

--- examples/nmr_solids/cp_contact_mas_nh_tdhamil.m
+++ examples/nmr_solids/cp_contact_mas_nh_tdhamil.m
@@ -1,6 +1,6 @@
 % Cross-polarisation experiment in the doubly rotating frame. A single
 % nitrogen-15 and a single proton. Spinning powder simulation starting
-% from the thermal equilibrium using Time-Dependent Hammiltonian Formalism.
+% from the thermal equilibrium using time-dependent Hamiltonian.
 %
 % Calculation time: seconds
 %
@@ -21,7 +21,7 @@ inter.coordinates={[0.00 0.00 0.00]
 inter.temperature=298;
 
 % Basis set
-bas.formalism='sphten-liouv';
+bas.formalism='zeeman-hilb';
 bas.approximation='none';
 
 % Spinach housekeeping
@@ -35,27 +35,28 @@ Hx=operator(spin_system,'Lx','1H');
 Hy=operator(spin_system,'Ly','1H');
 
 % MAS parameters
-parameters.rate=-10000;
+parameters.rate=10000;
 parameters.nperiods=10;
+parameters.max_rank=16;
 parameters.axis=[sqrt(2/3) 0 sqrt(1/3)];
-parameters.max_rank=4;
-parameters.npoints=parameters.nperiods*(2*parameters.max_rank+1);
 parameters.spins={'1H','15N'};
-parameters.irr_powers=[5e4*ones(1,parameters.npoints);
-                       4e4*ones(1,parameters.npoints)];
+parameters.irr_powers=[5e4 4e4];
 parameters.irr_opers={Hy Nx};
 parameters.exc_opers={Hx Ny};
 parameters.needs={'iso_eq'};
 parameters.coil=state(spin_system,'Lx','15N');
-parameters.grid='rep_2ang_800pts_sph';
-
-parameters.time_steps=ones(1,parameters.npoints)/(2*parameters.max_rank+1)*abs(1/parameters.rate);
+parameters.grid='rep_3ang_12800pts';
 
 % Simulation
 fid=powder(spin_system,@cp_contact_hard_tdhamil,parameters,'nmr');
 
+% Time axis generation
+rotor_period=1/parameters.rate;
+nintspp=2*parameters.max_rank+1;
+nsteps=nintspp*parameters.nperiods;
+time_axis=linspace(0,parameters.nperiods*rotor_period,nsteps+1);
+
 % Plot the answer
-time_axis=[0 cumsum(parameters.time_steps)];
 figure(); plot(time_axis,real(fid)); kgrid;
 kylabel('$S_{\rm{X}}$ expectation value on $^{15}N$');  
 kxlabel('time, seconds'); xlim tight;
--- experiments/cp_contact_hard_tdhamil.m
+++ experiments/cp_contact_hard_tdhamil.m
@@ -48,25 +48,19 @@
 
 function contact_curve=cp_contact_hard_tdhamil(spin_system,parameters,H,R,K)
 
-% Check consistency
-grumble(parameters,H,R,K);
+% Get pulse sequence timing
+rotor_period=1/parameters.rate;
+nintspp=2*parameters.max_rank+1;
+nsteps=nintspp*parameters.nperiods;
+dt=rotor_period/nintspp;
 
-% Preallocate contact curves
-contact_curve=zeros(size(parameters.coil,2),...
-                    numel(parameters.time_steps)+1);
+% Preallocate the observable array
+contact_curve=zeros([1 nsteps+1],'like',1i);
 
-% Preallocate operator stack depending on if we are using two or three angle
-% averaging grids
+% Get the rotor stack
+parameters.masframe='magnet';
 parameters.orientation=parameters.current_angles;
-if parameters.current_angles(1)==0
-    parameters.masframe='rotor';
-else
-    parameters.masframe='magnet';
-end    
-[L_stack,~] = rotor_stack(spin_system,parameters,'nmr');
-
-% Spatial dimension of the problem
-spc_dim=numel(L_stack);
+[L,~]=rotor_stack(spin_system,parameters,'nmr');
 
 % Build and project the excitation operator
 exc_oper=parameters.exc_opers{1};
@@ -74,83 +68,31 @@ for n=2:numel(parameters.exc_opers)
     exc_oper=exc_oper+parameters.exc_opers{n};
 end
 
-% Apply a perfect hard pi/2 excitation pulse
+% Build and project the spin-lock operator
+irr_oper=2*pi*parameters.irr_powers(1)*parameters.irr_opers{1};
+for k=2:numel(parameters.irr_opers)
+    irr_oper=irr_oper+2*pi*parameters.irr_powers(k)*parameters.irr_opers{k};
+end
+
+% Apply a perfect pi/2 excitation pulse
 rho=step(spin_system,exc_oper,parameters.rho0,pi/2);
 
-% Calculate initial magnetisation after 90d pulse
-contact_curve(:,1)=scalar_prod(spin_system,parameters.coil,rho);
-
-% Extend rho to cell if 2-angle grid is supplid
-if strcmp(parameters.masframe,'rotor')
-    rho_c=rho;
-    rho=cell(1,spc_dim);
-    for n=1:spc_dim
-        rho{n}=rho_c;
-    end
-end    
+% First point of the trajectory
+contact_curve(:,1)=hdot(parameters.coil,rho);
 
 % Loop over the time steps
-for n=1:parameters.npoints
+for n=1:nsteps
 
-    % Build and project the spin-lock operator
-    irr_oper=2*pi*parameters.irr_powers(1,n)*...
-        parameters.irr_opers{1};
-    for k=2:numel(parameters.irr_opers)
-        irr_oper=irr_oper+2*pi*parameters.irr_powers(k,n)*...
-            parameters.irr_opers{k};
-    end
+    % Get current evolution generator
+    G=L{mod(n-1,nintspp)+1}+irr_oper;
+    
+    % Take an evolution step
+    rho=step(spin_system,G,rho,dt);
 
-    % Propagate accordingly
-    if strcmp(parameters.masframe,'rotor')
-        for m=1:spc_dim
-            rho{m}=step(spin_system,L_stack{m}+irr_oper,rho{m},parameters.time_steps(n));
-            contact_curve(:,n+1)=contact_curve(:,n+1)+scalar_prod(spin_system,parameters.coil,rho{m})/(spc_dim);
-        end
-    else
-        rho=step(spin_system,L_stack{1}+irr_oper,rho,parameters.time_steps(n));
-        contact_curve(:,n+1)=scalar_prod(spin_system,parameters.coil,rho);
-    end
-
-    % Shift the Stack
-    L_stack=circshift(L_stack,sign(parameters.rate));
-end    
+    % Get the observable quantity
+    contact_curve(:,n+1)=hdot(parameters.coil,rho);
 
 end
 
-function res=scalar_prod(spin_system,A,B)
-    if ismember(spin_system.bas.formalism,{'zeeman-hilb'})
-        res=hdot(A,B);
-    else
-        res=A'*B;
-    end
-end
-
-% Consistency enforcement
-function grumble(parameters,H,R,K)
-if (~isnumeric(H))||(~isnumeric(R))||(~isnumeric(K))||...
-   (~ismatrix(H))||(~ismatrix(R))||(~ismatrix(K))
-    error('H, R and K arguments must be matrices.');
-end
-if ~isfield(parameters,'irr_powers')
-    error('nutation frequencies must be specified in parameters.irr_powers variable.');
-end
-if ~isfield(parameters,'irr_opers')
-    error('spin-lock operators must be specified in parameters.irr_opers variable.');
-end
-if ~iscell(parameters.irr_opers)
-    error('parameters.irr_opers must be a cell array of matrices.');
-end
-if ~isfield(parameters,'exc_opers')
-    error('excitation operators must be specified in parameters.exc_opers variable.');
-end
-if ~iscell(parameters.exc_opers)
-    error('parameters.exc_opers must be a cell array of matrices.');
-end
-if ~isfield(parameters,'rho0')
-    error('initial state must be specified in parameters.rho0 variable.');
-end
-if ~isfield(parameters,'coil')
-    error('detection state must be specified in parameters.coil variable.');
-end
 end
 

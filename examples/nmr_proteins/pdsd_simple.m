9a37ef57aa0057680590f0cca72913ce6fd4ff65
Ilya Kuprov <ilya.kuprov@weizmann.ac.il>
2025-04-29 13:17:18 +0300

    With able guidance from Guinevere Mathies, the PDSD sequence is working.

--- examples/nmr_proteins/pdsd_simple.m
+++ examples/nmr_proteins/pdsd_simple.m
@@ -1,10 +1,11 @@
-% 13C 2D PDSD spectrum of a simple text spin system.
+% 13C 2D PDSD spectrum of a simple test spin system.
 %
-% Calculation time: hours.
+% Calculation time: minutes, much faster on GPU.
 %
 % ilya.kuprov@weizmann.ac.il
+% guinevere.mathies@uni-konstanz.de
 
-function pdsd_simple_phase()
+function pdsd_simple()
 
 % Magnet field
 sys.magnet=21.1356;
@@ -12,19 +13,19 @@ sys.magnet=21.1356;
 % Isotopes
 sys.isotopes={'13C','1H','1H','13C'};
 
-% Interactions
+% Interactions (HCCH fragment)
 inter.zeeman.scalar={20.0 5.0 2.0 35.0};
-inter.coordinates={[0.00 0.00 -3.00],...
-                   [0.00 0.00 -1.00],...
-                   [0.00 0.00  1.00],...
-                   [0.00 0.00  3.00]};
+inter.coordinates={[-2.26  0.15  0.00],...
+                   [-1.90  0.66 -0.87],...
+                   [-0.67  0.88  1.26],...
+                   [-1.74  0.88  1.26]};
 
 % Basis set
 bas.formalism='sphten-liouv';
 bas.approximation='none';
 
 % Algorithmic options
-sys.enable={'greedy','gpu'};
+sys.enable={'prop_cache','gpu'};
 
 % Create the spin system structure
 spin_system=create(sys,inter);
@@ -34,21 +35,18 @@ spin_system=basis(spin_system,bas);
 
 % Experiment parameters
 parameters.spins={'1H','13C'};
-parameters.rate=100000;
-parameters.tmix=1e-3;
-parameters.max_rank=3;
+parameters.rate=10000;
+parameters.tmix=10e-3;
+parameters.max_rank=11;
 parameters.axis=[sqrt(2/3) 0 sqrt(1/3)];
-parameters.grid='rep_2ang_200pts_oct';
-parameters.npoints=[64 64];
+parameters.grid='rep_2ang_100pts_sph';
+parameters.npoints=[256 256];
 parameters.zerofill=[1024 1024];
 parameters.offset=[3150 6750];
 parameters.sweep=10000;
-parameters.verbose=1;
-parameters.serial=0;
-parameters.axis_units='ppm';
 
 % Simulation
-fid=singlerot(spin_system,@pdsd_phase,parameters,'nmr');
+fid=singlerot(spin_system,@pdsd,parameters,'nmr');
 
 % Apodisation
 fid.cos=apodisation(spin_system,fid.cos,{{'sqcos'},{'sqcos'}});
@@ -61,15 +59,16 @@ f1_sin=real(fftshift(fft(fid.sin,parameters.zerofill(2),1),1));
 % States signal
 f1_states=f1_cos-1i*f1_sin;
 
-% F1 Fourier transform
+% F1 Fourier transform and real part
 spectrum=fftshift(fft(f1_states,parameters.zerofill(1),2),2);
+spectrum=real(spectrum);
 
 % Plotting
 figure(); scale_figure([1.5 2.0]);
 parameters.spins={'13C'};
 parameters.offset=parameters.offset(2);
-plot_2d(spin_system,-real(spectrum),parameters,...
-        20,[0.05 0.25 0.05 0.25],2,256,6,'both'); 
+plot_2d(spin_system,spectrum,parameters,...
+        20,[0.05 0.25 0.05 0.25],2,256,6,'positive'); 
 
 end
 
--- experiments/nmr_solids/pdsd.m
+++ experiments/nmr_solids/pdsd.m
@@ -1,8 +1,11 @@
 % A simplified model of the PDSD experiment.
 %
+% ilya.kuprov@weizmann.ac.il
+% guinevere.mathies@uni-konstanz.de
+%
 % <https://spindynamics.org/wiki/index.php?title=pdsd.m>
 
-function fid=pdsd_phase(spin_system,parameters,H,R,K)
+function fid=pdsd(spin_system,parameters,H,R,K)
 
 % Compose the Liouvillian and its decoupled version
 L=H+1i*R+1i*K; LD=decouple(spin_system,L,[],{'1H'});
@@ -18,17 +21,14 @@ Cy=kron(speye(parameters.spc_dim),Cy);
 Hx=operator(spin_system,'Lx','1H');
 Hx=kron(speye(parameters.spc_dim),Hx);
 
-% Skip CP and start with Lz on carbons
-rho=state(spin_system,'Lz','13C','cheap');
-rho=kron(ones(parameters.spc_dim,1),rho);
-
 % Quadrature detection state
 coil=state(spin_system,'L+','13C','cheap');
 coil=kron(ones(parameters.spc_dim,1),coil);
 coil=coil/parameters.spc_dim;
 
-% First pulse
-rho=step(spin_system,Cx,rho,pi/2);
+% Skip CP and start with Ly on carbons
+rho=state(spin_system,'Ly','13C','cheap');
+rho=kron(ones(parameters.spc_dim,1),rho);
 
 % Phase cycle specification
 Op2={Cx,Cy,Cx,Cy}; An2={+pi/2,+pi/2,-pi/2,-pi/2};
@@ -40,8 +40,8 @@ fids=cell(1,4);
 % Phase cycle loop
 for n=1:4
 
-    % F1 evolution
-    rho_stack=evolution(spin_system,L,[],rho,timestep,...
+    % F1 evolution under proton decoupling
+    rho_stack=evolution(spin_system,LD,[],rho,timestep,...
                         parameters.npoints(1)-1,'trajectory');
     % Second pulse
     rho_stack=step(spin_system,Op2{n},rho_stack,An2{n});
@@ -54,9 +54,9 @@ for n=1:4
     rho_stack=step(spin_system,Op3{n},rho_stack,An3{n});
 
     % Wipe the proton subspace (decoupling)
-    % [~,rho_stack]=decouple(spin_system,[],rho_stack,{'1H'});
+    [~,rho_stack]=decouple(spin_system,[],rho_stack,{'1H'});
     
-    % F2 evolution and detection
+    % F2 evolution and detection under proton decoupling
     fids{n}=evolution(spin_system,LD,coil,rho_stack,timestep,...
                       parameters.npoints(2)-1,'observable');
                   

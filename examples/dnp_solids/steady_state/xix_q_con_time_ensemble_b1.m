dcebcdfbb0d7a432bbdf0a86ebf721fb945f1b48
Ilya Kuprov <ilya.kuprov@weizmann.ac.il>
2025-05-26 17:30:26 +0300

    precomputing expensive figures

--- examples/dnp_solids/steady_state/xix_q_con_time_ensemble_b1.m
+++ examples/dnp_solids/steady_state/xix_q_con_time_ensemble_b1.m
@@ -104,9 +104,9 @@ dnp=sum(dnp.*reshape(wb1,[1 numel(wb1)]),2)/sum(wb1);
 
 % Plotting 
 contact_times=parameters.pulse_dur*2*loop_counts;
-figure(); plot(contact_times*1e6,real(dnp),'-o');
+figure(); plot(contact_times*1e6,real(dnp));
 kylabel('$I_\textrm{z}$ expectation value on $^{1}$H');  
-kkxlabel('Total contact time, $\mu$s'); 
+kxlabel('Total contact time, $\mu$s'); 
 kgrid; xlim tight; ylim padded;
 
 % Save for later

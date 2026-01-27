7e439ac1dc0fb7f45db72cf2be22392768d78e9e
Ilya Kuprov <ilya.kuprov@weizmann.ac.il>
2026-01-27 11:01:54 +0200

    More stringent unit tests for the thermal equilibria.

--- examples/fundamentals/state_tests/thermal_equilibrium_1.m
+++ examples/fundamentals/state_tests/thermal_equilibrium_1.m
@@ -7,14 +7,14 @@ function thermal_equilibrium_1()
 
 % Spin system parameters
 sys.magnet=14.1;
-sys.isotopes={'E','1H','14N','15N'};
-inter.zeeman.scalar={2.0023 1.0 2.0 3.0};
+sys.isotopes={'E8','1H','14N','15N'};
+inter.zeeman.scalar={2.002319 1.0 2.0 3.0};
 inter.coupling.scalar=cell(length(sys.isotopes));
-inter.coupling.scalar{1,2}=1e6;
-inter.coupling.scalar{2,3}=1e6;
-inter.coupling.scalar{1,3}=1e3;
-inter.coupling.scalar{3,4}=1e3;
-inter.coupling.scalar{1,4}=1e2;
+inter.coupling.scalar{1,2}=1e6; % Remove these
+inter.coupling.scalar{2,3}=1e6; % couplings to
+inter.coupling.scalar{1,3}=1e3; % get machine
+inter.coupling.scalar{3,4}=1e3; % precision
+inter.coupling.scalar{1,4}=1e2; % match
 inter.temperature=4.2;
 
 % Preallocate the answers
@@ -28,16 +28,17 @@ for n=1:numel(formalisms)
     bas.approximation='none';
     spin_system=create(sys,inter);
     spin_system=basis(spin_system,bas);
-    rho=equilibrium(spin_system,hamiltonian(assume(spin_system,'labframe'),'left'));
-    eq_mags_spinach(1,n)=trace(state(spin_system,'Lz','E')'*rho);
+    rho=equilibrium(spin_system);
+    eq_mags_spinach(1,n)=trace(state(spin_system,'Lz','E8')'*rho);
     eq_mags_spinach(2,n)=trace(state(spin_system,'Lz','1H')'*rho);
     eq_mags_spinach(3,n)=trace(state(spin_system,'Lz','14N')'*rho);
     eq_mags_spinach(4,n)=trace(state(spin_system,'Lz','15N')'*rho);
 end
 
-% Get analytical equilibrium magnetisation
-[~,P]=levelpop('E',sys.magnet,inter.temperature);
-eq_mags_textbook(1)=0.5*P(1)-0.5*P(2);
+% Get analytical equilibrium magnetisations
+[~,P]=levelpop('E8',sys.magnet,inter.temperature);
+eq_mags_textbook(1)=+3.5*P(1)+2.5*P(2)+1.5*P(3)+0.5*P(4)...
+                    -0.5*P(5)-1.5*P(6)-2.5*P(7)-3.5*P(8);
 [~,P]=levelpop('1H',sys.magnet,inter.temperature);
 eq_mags_textbook(2)=0.5*P(1)-0.5*P(2);
 [~,P]=levelpop('14N',sys.magnet,inter.temperature);

68205a9f574f2e34fa8632177827b819f8533b31
Ilya Kuprov <ilya.kuprov@weizmann.ac.il>
2026-01-27 11:58:19 +0200

    Cavity energy level population dynamics for the spin-cavity example.

--- examples/quantum_tech/jaynes_cummings_b.m
+++ examples/quantum_tech/jaynes_cummings_b.m
@@ -38,8 +38,10 @@ delta=2*pi*5e6;
 % Rotating frame Hamiltonian
 H=delta*Ez+H_JC;
 
-% Initial state - Sx in empty cavity mode
-rho=state(spin_system,{'Lx','BL1'},{1,2});
+% Initial state - uncorrelated Sx on spin
+% and the cavity mode initially empty
+rho=state(spin_system,{'Lx','E'  },{1,2})+...
+    state(spin_system,{'E' ,'BL1'},{1,2})/2;
 
 % Detection state, spin
 S=state(spin_system,{'Lx'},{1});
@@ -48,7 +50,7 @@ S=state(spin_system,{'Lx'},{1});
 B=(state(spin_system,{'C'},{2})-...
    state(spin_system,{'A'},{2}))/2i;
 
-% Run evolution
+% Run observables evolution
 traj_s=evolution(spin_system,H,S,rho,10e-9,250,'observable');
 traj_c=evolution(spin_system,H,B,rho,10e-9,250,'observable');
 
@@ -63,6 +65,22 @@ subplot(1,2,2); plot(time_axis,real(traj_c));
 axis tight; kgrid; kxlabel('time, $\mu$s');
 kylabel('$(a^{+}-a)/2i$ of the mode');
 ktitle('cavity mode dynamics');
+
+% Cavity energy level population evolution
+L1=state(spin_system,{'E','BL1'},{1,2});
+L1=evolution(spin_system,H,L1,rho,10e-9,250,'observable');
+L2=state(spin_system,{'E','BL2'},{1,2});
+L2=evolution(spin_system,H,L2,rho,10e-9,250,'observable');
+L3=state(spin_system,{'E','BL3'},{1,2});
+L3=evolution(spin_system,H,L3,rho,10e-9,250,'observable');
+L4=state(spin_system,{'E','BL4'},{1,2});
+L4=evolution(spin_system,H,L4,rho,10e-9,250,'observable');
+L5=state(spin_system,{'E','BL5'},{1,2});
+L5=evolution(spin_system,H,L5,rho,10e-9,250,'observable');
+kfigure(); plot(time_axis,real([L1 L2 L3 L4 L5])); 
+xlim tight; ylim padded; kxlabel('time, $\mu$s');
+kylabel('cavity level population'); kgrid;
+klegend({'L1','L2','L3','L4','L5'},'Location','East');
 
 end
 

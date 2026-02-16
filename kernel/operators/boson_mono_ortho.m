50cc94728faff313316e8c92e0282d24b3b07b61
sarbojoy <das.sarbojoy@gmail.com>
2026-02-16 14:26:58 +0200

    Minor changes by Kuprov

--- kernel/operators/boson_mono_ortho.m
+++ kernel/operators/boson_mono_ortho.m
@@ -1,30 +1,23 @@
-function B = boson_mono_ortho(nlevels)
+function B_new = boson_mono_ortho(nlevels)
 
 % Calling the bosonic monomials to be orthogonalized
-Bm = boson_mono(nlevels);
+B_old = boson_mono(nlevels);
 
 % Extracting number of elements
-N = numel(Bm); 
+N = numel(B_old); 
 
 % Preallocating cell array for orthogonalized bosonic monomials
-B = cell(N,1);
-
-% Tolerance for numerical linear dependence
-tol = 1e-12;
+B_new = cell(N,1);
 
 % Gram-Schmidt orthogonalization
 for n = 1:N
-    total_proj = sparse(zeros(nlevels)); 
+    total_proj = spalloc(nlevels,nlevels,0); 
     for k = 1:(n-1)
-        denom = hdot(B{k},B{k});
-        % Checking if the norm in the denominator is stable
-        if abs(denom)<tol
-            continue;
-        end    
-        sub_proj = (hdot(B{k},Bm{n})/denom)*B{k};
+        denom = hdot(B_new{k},B_new{k});
+        sub_proj = (hdot(B_new{k},B_old{n})/denom)*B_new{k};
         total_proj = total_proj + sub_proj;
     end
-        B{n} = Bm{n} - total_proj; % Storing orthogonal monomials 
+        B_new{n} = B_old{n} - total_proj; % Storing orthogonal monomials 
 
 end
 

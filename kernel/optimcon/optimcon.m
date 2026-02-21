167faf92410c120c3cea2935081e8da9701f8ccc
Ilya Kuprov <ilya.kuprov@weizmann.ac.il>
2026-02-21 15:24:10 +0200

    Hessian has no business being even moderately ill-conditioned in the GRAPE context.

--- kernel/optimcon/optimcon.m
+++ kernel/optimcon/optimcon.m
@@ -1192,9 +1192,9 @@ spin_system.control.ls_tau1=3;         % Line search: bracket expansion factor
 spin_system.control.ls_tau2=0.1;       % Line search: left section contraction
 spin_system.control.ls_tau3=0.5;       % Line search: right section contraction
 spin_system.control.reg_max_iter=2500; % RFO: max regularisation iterations
-spin_system.control.reg_alpha=1;       % RFO: scaling factor
-spin_system.control.reg_phi=0.9;       % RFO: conditioning multiplier
-spin_system.control.reg_max_cond=1e4;  % RFO: max condition number
+spin_system.control.reg_alpha=1;       % RFO: initial scaling factor
+spin_system.control.reg_phi=0.5;       % RFO: conditioning multiplier
+spin_system.control.reg_max_cond=1e3;  % RFO: max condition number
 
 % Accept pulse sequence parameters
 if isfield(control,'parameters')

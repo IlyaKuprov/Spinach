65855314d54fdc63d9a9964decf0a4cd6a03c2d3
Ilya Kuprov <ilya.kuprov@weizmann.ac.il>
2026-05-19 19:56:12 +0300

    Differentiation behaviour observed by Alexey is thankfully not a bug.

---
+++ examples/quantum_tech/diamond_defects/diamond_ninter_epr_xw.m
@@ -0,0 +1,66 @@
+% Field-swept powder EPR spectra of a nitrogen interstitial
+% defect in diamond at X and W bands.
+%
+% alexey.bogdanov@weizmann.ac.il
+
+function diamond_ninter_epr_xw()
+
+% Set WAR9 nitrogen interstitial centre parameters
+ninter_params.orientation='111';
+ninter_params.nitrogen='15N';
+ninter_params.centre='war9';
+
+% Build the spin system
+[sys,inter]=diamond_n_inter(ninter_params);
+
+% Field sweep
+sys.magnet=1;
+
+% Define the basis set
+bas.formalism='zeeman-hilb';
+bas.approximation='none';
+
+% Run Spinach housekeeping
+spin_system=create(sys,inter);
+spin_system=basis(spin_system,bas);
+
+% EPR sim parameters
+parameters.spins={'E'};
+parameters.grid=4;
+parameters.fwhm=0.00001;
+parameters.int_tol=0.01;
+parameters.tm_tol=0.01;
+parameters.npoints=1024;
+parameters.rspt_order=Inf;
+
+% Set X-band parameters
+parameters.mw_freq=9.755e9;
+parameters.window=[0.347 0.349];
+
+% Run the X-band simulation
+[b_axis_x,spec_x]=fieldsweep(spin_system,parameters);
+
+% Plot the X-band spectrum
+kfigure(); scale_figure([1.50 0.75]);
+subplot(1,2,1); plot(b_axis_x,spec_x);
+kxlabel('magnetic field, tesla');
+kylabel('intensity, a.u.');
+ktitle('WAR9 N interstitial X-band EPR');
+xlim tight; ylim padded; kgrid;
+
+% Set W-band parameters
+parameters.mw_freq=94e9;
+parameters.window=[3.351 3.355];
+
+% Run the W-band simulation
+[b_axis_w,spec_w]=fieldsweep(spin_system,parameters);
+
+% Plot the W-band spectrum
+subplot(1,2,2); plot(b_axis_w,spec_w);
+kxlabel('magnetic field, tesla');
+kylabel('intensity, a.u.');
+ktitle('WAR9 N interstitial W-band EPR');
+xlim tight; ylim padded; kgrid;
+
+end
+

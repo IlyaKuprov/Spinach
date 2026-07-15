e038a8d67d2d0db03c02ee77399e7de3562cc92b
Ilya Kuprov <ilya.kuprov@weizmann.ac.il>
2026-07-15 10:09:33 +0300

    Methyl TROSY example improvements.

--- examples/relaxation_theory/trosy_methyl.m
+++ examples/relaxation_theory/trosy_methyl.m
@@ -1,7 +1,6 @@
 % Methyl trosy in a rapidly rotating 13CH3 group
 % of a slowly tumbling protein, simulated using 
-% the Fokker-Planck formalism. Alanine is used
-% as a geometric model.
+% the Fokker-Planck formalism.
 %
 % ilya.kuprov@weizmann.ac.il
 
@@ -10,20 +9,11 @@ function trosy_methyl()
 % Magnet field
 sys_big.magnet=14.1;
 
-% Cartesian coordinates: CA carbon
-ca_xyz=[-0.584588 0.114350 0.394793];
-
-% Cartesian coordinates: Me group
-me_xyz=[-1.290651 1.279824 -0.248354;
-        -1.237804 1.209518 -1.347802;
-        -2.348228 1.329796 0.051440;
-        -0.799560 2.212894 0.057684];
-
-% Cartesian coordinates: Me carbon
-cb_xyz=me_xyz(1,:);
-
-% Methyl group turning axis
-turning_axis=cb_xyz-ca_xyz;
+% Cartesian coordinates
+me_xyz=[-1.290651 1.279824 -0.248354;  % C
+        -1.237804 1.209518 -1.347802;  % H
+        -2.348228 1.329796  0.051440;  % H
+        -0.799560 2.212894  0.057684]; % H
 
 % Absolute shielding tensors (DFT)
 shielding=cell(1,4);
@@ -41,89 +31,90 @@ shielding{4}=[ 28.8    1.3    1.0;
                 1.0    1.4   25.7];
 
 % Convert shielding tensors into 
-% chemical shift tensors
-shift_ref=[188.0 31.8 31.8 31.8];
-inter.coordinates=num2cell(me_xyz,2);
-inter.zeeman.matrix=cell(1,5);
+% chemical shift tensors and put
+% them on resonance
+shift_tensor=cell(1,4);
 for n=1:4
-
-    % Convert chemical shielding to chemcial shift
-    inter.zeeman.matrix{n}=shift_ref(n)*eye(3)-shielding{n};
-
-    % Put chemical shift on resonance with transmitter
-    inter.zeeman.matrix{n}=remtrace(inter.zeeman.matrix{n});
-
+    shift_tensor{n}=-remtrace(shielding{n});
 end
 
-% J-couplings for the methyl group
-inter.coupling.scalar=cell(4,4);
-inter.coupling.scalar{1,2}=125;
-inter.coupling.scalar{1,3}=125;
-inter.coupling.scalar{1,4}=125;
-inter.coupling.scalar{2,3}=-12;
-inter.coupling.scalar{2,4}=-12;
-inter.coupling.scalar{3,4}=-12;
+% Methyl proton chemical shifts (guess)
+shift_tensor{2}=shift_tensor{2}+0.8*eye(3);
+shift_tensor{3}=shift_tensor{3}+1.0*eye(3);
+shift_tensor{4}=shift_tensor{4}+1.2*eye(3);
+
+% J-couplings
+j_coupling=cell(4,4);
+j_coupling{1,2}=125;
+j_coupling{1,3}=125;
+j_coupling{1,4}=125;
+j_coupling{2,3}=-12;
+j_coupling{2,4}=-12;
+j_coupling{3,4}=-12;
 
 % Spin system instances for the three methyl rotamers
 sys_big.isotopes=repmat({'13C','1H','1H','1H'},1,3);
-inter_big.coordinates=cell(1,12);
-inter_big.zeeman.matrix=cell(1,12);
-inter_big.coupling.scalar=cell(12,12);
+inter.coordinates=cell(1,12);
+inter.zeeman.matrix=cell(1,12);
+inter.coupling.scalar=cell(12,12);
 
-% Rotamers
-for r=1:3
+% Assign spins to rotamers
+inter.chem.parts={[1 2 3 4], [5 6 7 8], [9 10 11 12]};
 
-    % Get the methyl group turning matrix
-    R_me=anax2dcm(turning_axis,2*pi*(r-1)/3);
+% Equal rotamer populations
+inter.chem.concs=[1 1 1];
 
-    % Turn the methyl group
-    current_xyz=(me_xyz-cb_xyz)*R_me'+cb_xyz;
+% Interactions within methyl rotamer A
+inter.coordinates{1}=me_xyz(1,:);
+inter.coordinates{2}=me_xyz(2,:);
+inter.coordinates{3}=me_xyz(3,:);
+inter.coordinates{4}=me_xyz(4,:);
+inter.zeeman.matrix{1}=shift_tensor{1};
+inter.zeeman.matrix{2}=shift_tensor{2};
+inter.zeeman.matrix{3}=shift_tensor{3};
+inter.zeeman.matrix{4}=shift_tensor{4};
+inter.coupling.scalar(1:4,1:4)=j_coupling;
 
-    % Assign atoms
-    for n=1:4
-        inter_big.coordinates{(r-1)*4+n}=current_xyz(n,:);
-    end
+% Interactions within methyl rotamer B
+inter.coordinates{5}=me_xyz(1,:);
+inter.coordinates{6}=me_xyz(4,:);
+inter.coordinates{7}=me_xyz(2,:);
+inter.coordinates{8}=me_xyz(3,:);
+inter.zeeman.matrix{5}=shift_tensor{1};
+inter.zeeman.matrix{6}=shift_tensor{4};
+inter.zeeman.matrix{7}=shift_tensor{2};
+inter.zeeman.matrix{8}=shift_tensor{3};
+inter.coupling.scalar(5:8,5:8)=j_coupling;
 
-    % Assign CSAs
-    for n=1:4
-        inter_big.zeeman.matrix{(r-1)*4+n}=R_me*inter.zeeman.matrix{n}*R_me';
-    end
-
-    % Assign J-couplings
-    for n=1:4
-        for m=1:4
-            inter_big.coupling.scalar{(r-1)*4+n,(r-1)*4+m}=inter.coupling.scalar{n,m};
-        end
-    end
-
-end
+% Interactions within methyl rotamer C
+inter.coordinates{9}=me_xyz(1,:);
+inter.coordinates{10}=me_xyz(3,:);
+inter.coordinates{11}=me_xyz(4,:);
+inter.coordinates{12}=me_xyz(2,:);
+inter.zeeman.matrix{9}=shift_tensor{1};
+inter.zeeman.matrix{10}=shift_tensor{3};
+inter.zeeman.matrix{11}=shift_tensor{4};
+inter.zeeman.matrix{12}=shift_tensor{2};
+inter.coupling.scalar(9:12,9:12)=j_coupling;
 
 % Formalism and basis set
 bas.formalism='sphten-liouv';
 bas.approximation='none';
-bas.projections=+1;
-bas.zero_quantum={'1H'};
 
-% Assign spins to rotamers
-inter_big.chem.parts=cell(1,3);
-for n=1:3
-    inter_big.chem.parts{n}=4*(n-1)+(1:4);
-end
-
-% Equal rotamer populations
-inter_big.chem.concs=[1 1 1];
-
-% Methyl jump generator
+% Methyl turning generator
 tau_m=1e-11; k_jump=1/(2*tau_m);
-inter_big.chem.rates=k_jump*[-2  1  1;
-                              1 -2  1;
-                              1  1 -2];
+inter.chem.rates=k_jump*[-2  1  1;
+                          1 -2  1;
+                          1  1 -2];
 
 % Spinach housekeeping
-spin_system=create(sys_big,inter_big);
+spin_system=create(sys_big,inter);
 spin_system=basis(spin_system,bas);
 
-% Spectrum parameters
+% Get a figure going
+kfigure(); scale_figure([1.5 0.8]);
+
+% Parameters: 13C NMR
 parameters.tau_c=50e-9;
 parameters.max_rank=3;
 parameters.rho0=state(spin_system,'L+','13C');
@@ -131,6 +122,23 @@ parameters.coil=state(spin_system,'L+','13C');
 parameters.decouple={};
 parameters.spins={'13C'};
 parameters.sweep=[-300 300];
+parameters.npoints=1024;
+parameters.axis_units='Hz';
+
+% Call frequency domain detection
+spectrum=gridfree(spin_system,@slowpass,parameters,'nmr');
+
+% Do the plotting
+subplot(1,2,1); plot_1d(spin_system,real(spectrum),parameters);
+
+% Parameters: 1H NMR
+parameters.tau_c=50e-9;
+parameters.max_rank=3;
+parameters.rho0=state(spin_system,'L+','1H');
+parameters.coil=state(spin_system,'L+','1H');
+parameters.decouple={};
+parameters.spins={'1H'};
+parameters.sweep=[200 1000];
 parameters.npoints=2048;
 parameters.axis_units='Hz';
 
@@ -138,7 +146,7 @@ parameters.axis_units='Hz';
 spectrum=gridfree(spin_system,@slowpass,parameters,'nmr');
 
 % Do the plotting
-kfigure(); plot_1d(spin_system,real(spectrum),parameters);
+subplot(1,2,2); plot_1d(spin_system,real(spectrum),parameters);
 
 end
 
